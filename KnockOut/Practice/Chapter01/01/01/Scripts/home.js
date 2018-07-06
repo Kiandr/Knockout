566e578ff4f38920ce2eede35f4deb988313ba90
kiandr <kian.rad@enlivenfinancial.com>
2018-07-06 15:48:22 -0700

    Double Way KO.Observable were studied. KO.Computed was also tested.

---
+++ KnockOut/Practice/Chapter01/01/01/Scripts/home.js
@@ -0,0 +1,25 @@
+﻿// Test the jquery works
+console.log("Hello World");
+
+// Step0: make sure that you invoke the method in an intial function
+// add data binder step1
+$(function () {
+    var viewModel = {
+        firstName: ko.observable("Kian"),
+        lastName: ko.observable("Rad"),
+        //emailAdderss: ko.computed(function() {
+        //    return (this.firstName() + " - > ");
+        //    })
+            
+    };
+    viewModel.emailAdderss = ko.computed(function() {
+        return (this.firstName().toString().toLowerCase() + "." + this.lastName().toLowerCase()+"@enlivenfinancial.com");
+    }, viewModel);
+    ko.applyBindings(viewModel);
+    $("#test").click(function () {
+        console.log(viewModel.firstName());
+        viewModel.firstName("Omid");
+        console.log(viewModel.firstName());
+    });
+});
+
