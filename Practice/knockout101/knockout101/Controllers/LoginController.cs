b979b3df3d7fc99a712cc73d0ac866b11b6ec2d6
Kian Davoudi-Rad <kiandr@KIAN.hitronhub.home>
2018-09-17 21:05:27 -0700

    adding lates work

---
+++ Practice/knockout101/knockout101/Controllers/LoginController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace knockout101
+{
+    public class LoginController : Controller
+    {
+
+        //Get:    /Login/Index
+        public ActionResult Index()
+        {
+           
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+
+            //var firstName = "Elmira";
+            //var passWord = "1234";
+            //var result = firstName + passWord;
+            //return Json(result, JsonRequestBehavior.AllowGet);
+
+
+            return Json("success", JsonRequestBehavior.AllowGet);
+
+
+
+
+
+
+
+        }
+
+
+      
+    }
+}
